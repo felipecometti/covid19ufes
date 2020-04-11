7ace98c5e85cc37921091aed9b433da7713597c0
felipecometti <edkhod@hotmail.com>
2020-04-11 01:15:30 -0300

    first files for the impostometro extractor

---
+++ im-extract.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import calendar as cd
+import requests as rq
+import os
+
+# Read cities.csv into a list
+
+# Open file where the data will be stored
+#f = open("im_es.csv", "a")
+
+# IMPORTANT start from the inside of the loop
+# Loop through cities list
+    # Loop through AAAA-MM
+        # For the GET request: use interval from 01 to lastday = cd.monthrange(2010, 1)[1]
+        # Send GET request to site via requests
+        # Read json string to a dataframe pd.read_json(contents)
+        # Get value out of "Valor"
+        # write to file a line as AAAA-MM-01,<Valor>\n
