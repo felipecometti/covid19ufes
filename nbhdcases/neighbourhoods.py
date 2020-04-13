4b358de7ed23e9ab29a83b705f5ef15aba44bbd0
felipecometti <edkhod@hotmail.com>
2020-04-13 17:18:31 -0300

    now verifies if input date is not already there

--- nbhdcases/neighbourhoods.py
+++ nbhdcases/neighbourhoods.py
@@ -1,15 +1,22 @@
 import pandas as pd
 
-url = "https://coronavirus.es.gov.br/distribuicao-dos-casos-confirmados-da-covid-19-por-bairro"
-df_list = pd.read_html(url)                     # Still getting used to pandas
-df = df_list[-1]                                # Get last avaliable table
-df.columns = df.iloc[0]                         # Define row 0 as headers
-df = df.drop([0])                               # Drop row 0
-df = df.dropna()                                # Drop NaNs
-
 # Create date column (to be automated in the future)
 get_date = input("Qual o dia de atualização (em ??/04/2020)? ")
 get_date = get_date + "/04/2020"
-df["Data"]=get_date
 
-df.to_excel("CasosBairros.xlsx", sheet_name="Casos Bairros", index=False)
+# Load file to append, check if date is not already there
+df_file = pd.read_excel("CasosBairros.xlsx")
+if get_date not in df_file['Data'].tolist():
+    # Get table from website
+    url = "https://coronavirus.es.gov.br/distribuicao-dos-casos-confirmados-da-covid-19-por-bairro"
+    df_list = pd.read_html(url)                     # Still getting used to pandas
+    df = df_list[-1]                                # Get last avaliable table
+    df.columns = df.iloc[0]                         # Define row 0 as headers
+    df = df.drop([0])                               # Drop row 0
+    df = df.dropna()                                # Drop NaNs
+    df["Data"]=get_date
+
+    df_file = df_file.append(df)
+    df_file.to_excel("CasosBairros.xlsx", sheet_name="Casos Bairros", index=False)
+else:
+    print("A data já está na tabela.")
