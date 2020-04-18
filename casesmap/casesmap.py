33fcd8b4262c75c79fe5e82932176f4932149dc3
felipecometti <edkhod@hotmail.com>
2020-04-18 03:34:41 -0300

    first successful run of casesmap

--- casesmap/casesmap.py
+++ casesmap/casesmap.py
@@ -1,15 +1,49 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import colors
 
-df = pd.read_csv('nyc_taxi.csv', usecols=['dropoff_x', 'dropoff_y'])
+# Variables to mess with
+the_bins = 200
+the_cmap = "gist_heat" # "Reds" for light background
 
-x = df['dropoff_x'].tolist()
-y = df['dropoff_y'].tolist()
+# Load caso_full.csv
+cases = pd.read_csv("caso_full.csv", usecols=['city_ibge_code', 'date',\
+    'last_available_confirmed', 'place_type'])
+cases = cases[cases['place_type'] == "city"]
+the_vmax = cases.loc[cases['last_available_confirmed'].idxmax()]
+the_vmax = the_vmax.last_available_confirmed
 
-plt.figure(num=None, figsize=(11, 8), dpi=300, facecolor='w', edgecolor='w')
-plt.hist2d(x, y, bins=1000, norm=colors.PowerNorm(0.3), cmap='gist_heat')
-plt.xlim(-80, -30)
-plt.ylim(-36, 8)
-plt.axis("off")
-plt.savefig('output21.png', bbox_inches=0)
+# Load municipios.csv
+cities = pd.read_csv("municipios.csv", usecols=['codigo_ibge', 'latitude', 'longitude'])
+
+# Combine dfs by city_ibge_code | codigo_ibge
+merged = pd.merge(cases, cities, how='left', left_on='city_ibge_code', right_on='codigo_ibge')
+merged = merged.dropna()
+
+# Create dates list
+dates = merged['date'].drop_duplicates()
+dates = dates.tolist()
+
+for the_date in dates:
+    print(the_date)
+    to_plot = merged[merged['date'] == the_date]
+    x = to_plot['longitude'].tolist()
+    y = to_plot['latitude'].tolist()
+    the_weights = to_plot['last_available_confirmed'].to_numpy()
+
+    # Drawing map
+    img = plt.figure(num=None, figsize=(20, 20), dpi=300, facecolor='w', edgecolor='w')
+    plt.hist2d(x, y, bins=the_bins, norm=colors.PowerNorm(0.3, vmin=0, vmax=the_vmax),\
+        cmap=the_cmap, weights=the_weights)
+    plt.xlim(-75.2, -28.75)
+    plt.ylim(-34.1, 5.6)
+    plt.gca().set_facecolor('#000000')
+    plt.gca().set_aspect('equal', adjustable='box')
+    plt.gca().axes.xaxis.set_visible(False)
+    plt.gca().axes.yaxis.set_visible(False)
+    plt.gca().axes.xaxis.set_ticklabels([])
+    plt.gca().axes.yaxis.set_ticklabels([])
+    img.add_subplot().text(-29.75, -33.1, the_date, fontsize=28, color='white', horizontalalignment='right')
+    plt.savefig(the_date + '.png', bbox_inches=0)
+    plt.close()
