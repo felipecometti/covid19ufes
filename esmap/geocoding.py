9aec9bdd047ac327d940723f6b37bcb37ac54d9d
felipecometti <edkhod@hotmail.com>
2020-04-26 17:35:07 -0300

    defined geocoding.py processes

---
+++ esmap/geocoding.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import googlemaps
+
+# read locations into a dataframe
+#create new lat and long columns
+# loop over the locations
+    # for location in df['column with the strings']
+    # add the coordinates to lat and long
+#write the result to_excel()
