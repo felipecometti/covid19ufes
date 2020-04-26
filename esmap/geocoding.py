0090bfafcd2a4ffaf5998b4c0e749b5ae53e9526
felipecometti <edkhod@hotmail.com>
2020-04-26 18:27:23 -0300

    changes to .gitignore and inputs for geocoding.py

--- esmap/geocoding.py
+++ esmap/geocoding.py
@@ -1,7 +1,12 @@
 import pandas as pd
 import googlemaps
 
+# initializing google maps
+gkey = pd.read_csv("googleapi.csv")['key'].iloc[0]
+gmaps = googlemaps.Client(key=gkey)
+
 # read locations into a dataframe
+df_nbhd = 0
 #create new lat and long columns
 # loop over the locations
     # for location in df['column with the strings']
