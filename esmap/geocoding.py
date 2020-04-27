ebef8222134a467ccc76b165b79a577a7123bc52
felipecometti <edkhod@hotmail.com>
2020-04-26 23:22:22 -0300

    geocoding.py runs successfully

--- esmap/geocoding.py
+++ esmap/geocoding.py
@@ -1,14 +1,20 @@
 import pandas as pd
 import googlemaps
+import json
 
 # initializing google maps
 gkey = pd.read_csv("googleapi.csv")['key'].iloc[0]
 gmaps = googlemaps.Client(key=gkey)
 
-# read locations into a dataframe
-df_nbhd = 0
-#create new lat and long columns
-# loop over the locations
-    # for location in df['column with the strings']
-    # add the coordinates to lat and long
-#write the result to_excel()
+# read neighbourhood names into a dataframe
+df_nbhd = pd.read_csv("nbhd.csv")
+total_nbhd = len(df_nbhd.index)
+
+for x in range(0, total_nbhd):
+    location = df_nbhd['M+B'].iloc[x]
+    print(x, location)
+    request = gmaps.geocode(location)
+    df_nbhd.loc[df_nbhd.index[x], 'latitude'] = request[0]['geometry']['location']['lat']
+    df_nbhd.loc[df_nbhd.index[x], 'longitude'] = request[0]['geometry']['location']['lng']
+
+df_nbhd.to_csv("nbhd.csv", index=False)
